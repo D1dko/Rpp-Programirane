63645c3c77d0a69256f08d470a90f954b0318462
D1dko <deqnyordanov@mail.com>
2023-02-22 09:34:43 +0200

    upload project

---
+++ 8aupr1/8aupr1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _8aupr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double grade = double.Parse(Console.ReadLine());
+             if (grade >= 5.50) 
+            {
+                Console.WriteLine("poluchavash stipendiq");
+                    }
+                else
+            {
+                Console.WriteLine("FILIP");
+                    }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ListAction/ListAction/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListAction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+        }
+    }
+}
